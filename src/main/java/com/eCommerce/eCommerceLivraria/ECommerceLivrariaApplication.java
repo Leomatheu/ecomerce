a31334105dd2ae607721237156bdd1725bf46e5e
LEONARDO MATHEUS DE FREITAS ROA <leonardo.roa@alunos.sc.senac.br>
2022-11-07 20:12:21 -0300

    primeiro commit

---
+++ src/main/java/com/eCommerce/eCommerceLivraria/ECommerceLivrariaApplication.java
@@ -0,0 +1,16 @@
+package com.eCommerce.eCommerceLivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.Entity;
+
+
+@SpringBootApplication
+public class ECommerceLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceLivrariaApplication.class, args);
+	}
+
+}
